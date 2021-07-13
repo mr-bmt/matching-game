f8ea61a937914c341772048a4638345c10086872
Bruno M. Trajano <bmtest@gmail.com>
2021-07-13 17:08:25 -0300

    :sparkles: Add card flipping behavior

--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -2,10 +2,20 @@ import "./styles.css"
 
 function GameCard() {
   return /*html*/ `
-    <figure class="game-card">
-      <img src="images/alura-pixel.png" alt="Logo da Alura" class="icon" />
-    </figure>
+    <div class="game-card-box">
+      <div class="game-card" onclick="this.classList.toggle('is-flipped')">
+        <figure class="game-card-face game-card-face-front">
+          <img src="images/alura-pixel.png" alt="Logo da Alura" class="icon" />
+        </figure>
+        <figure class="game-card-face game-card-face-back">
+          <img src="images/logo-javascript.png" alt="Logo da Javascript" class="icon" />
+        </figure>
+      </div>
+    </div>
+  
   `;
 }
+
+
 
 export default GameCard;
